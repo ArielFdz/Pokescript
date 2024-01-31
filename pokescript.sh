89933e6959bd83b0be4302cf8f597e1905862a86
ArielFdz <arieljr11@outlook.com>
2024-01-30 22:57:40 -0600

    Add the script

---
+++ pokescript.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -z "$1" ] 
+then
+    echo "Se necesita indicar el nombre del pokemon";
+else
+    api_data=$(curl -s "https://pokeapi.co/api/v2/pokemon/$1");
+
+    if [ "$api_data" = "Not Found" ]
+    then 
+        echo "Pokemon incorrecto";
+    else
+        id=$(echo "$api_data" | jq '.id');
+        name=$(echo "$api_data" | jq '.name');
+        weight=$(echo "$api_data" | jq '.weight');
+        height=$(echo "$api_data" | jq '.height');
+        order=$(echo "$api_data" | jq '.order');
+
+        echo "$name (No. $id)";
+        echo "Id = $id";
+        echo "Weight = $weight";
+        echo "Height = $height";
+        echo "Order = $order";
+    fi
+fi
+exit;
+
