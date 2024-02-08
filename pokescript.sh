33a2becb093ceb11b92aa3757ee4fb837620a677
ArielFdz <arieljr11@outlook.com>
2024-02-07 19:33:26 -0600

    fixed bug

--- pokescript.sh
+++ pokescript.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-if [ -z "$1" ] 
-then
+if [ -z "$1" ]; then
     echo "Se necesita indicar el nombre del pokemon";
-else
-    api_data=$(curl -s "https://pokeapi.co/api/v2/pokemon/$1");
-
-    if [ "$api_data" = "Not Found" ]
-    then 
-        echo "Pokemon incorrecto";
-    else
-        id=$(echo "$api_data" | jq '.id');
-        name=$(echo "$api_data" | jq '.name');
-        weight=$(echo "$api_data" | jq '.weight');
-        height=$(echo "$api_data" | jq '.height');
-        order=$(echo "$api_data" | jq '.order');
-
-        echo "$name (No. $id)";
-        echo "Id = $id";
-        echo "Weight = $weight";
-        echo "Height = $height";
-        echo "Order = $order";
-    fi
+    exit
 fi
+
+api_data=$(curl -s "https://pokeapi.co/api/v2/pokemon/$1");
+
+if [ "$api_data" = "Not Found" ] || [ -z "$api_data" ]; then 
+    echo "Pokemon incorrecto";
+else
+    id=$(echo "$api_data" | jq '.id');
+    name=$(echo "$api_data" | jq '.name');
+    weight=$(echo "$api_data" | jq '.weight');
+    height=$(echo "$api_data" | jq '.height');
+    order=$(echo "$api_data" | jq '.order');
+
+    echo "$name (No. $id)";
+    echo "Id = $id";
+    echo "Weight = $weight";
+    echo "Height = $height";
+    echo "Order = $order";
+fi
+
 exit;
+
 
